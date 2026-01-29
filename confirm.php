2e54fe9e744c28d864cc3d6173a094b9979f38af
StefanT2024 <stefantujarov24@gmail.com>
2026-01-29 15:08:07 -0600

    Add PHP form files

---
+++ confirm.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Submitted Details:</h2>
+<?php
+echo "Name: " . $_POST["name"] . "<br>";
+echo "Email: " . $_POST["email"] . "<br>";
+echo "Age: " . $_POST["age"] . "<br>";
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="confirm.php" method="post">
+  Name: <input type="text" name="name"><br>
+  Email: <input type="email" name="email"><br>
+  Age: <input type="number" name="age"><br>
+  <input type="submit">
+</form>
+</body>
+</html>
