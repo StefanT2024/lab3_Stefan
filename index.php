863ed62894608b41465c7dff897e1d4d9bb379ab
Stefan Tujarov <stefantujarov24@gmail.com>
2026-01-29 15:31:59 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <body>
+<?php
+echo "<p>Hello from the internet!</p>";
+?>
 <form action="confirm.php" method="post">
   Name: <input type="text" name="name"><br>
   Email: <input type="email" name="email"><br>
